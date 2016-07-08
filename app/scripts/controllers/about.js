85722b10de0e88978d541f45f38cae781503a482
Leon Ling <lingl@hsntech.com>
2016-07-08 15:05:48 +0800

    initial work files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teamBlogApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the teamBlogApp
+ */
+angular.module('teamBlogApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teamBlogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the teamBlogApp
+ */
+angular.module('teamBlogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
